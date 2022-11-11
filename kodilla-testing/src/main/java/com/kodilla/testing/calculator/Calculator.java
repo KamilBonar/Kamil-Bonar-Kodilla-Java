668c2156a84af06d9c9ca9217bea603987f00563
Kamil Bonar <kamilbr@op.pl>
2022-11-11 22:09:54 +0100

    6.2 TDD calculator

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        int suma = a + b;
+        return suma;
+    }
+
+    public int subtract(int a, int b){
+        int roznica = a - b;
+        return roznica;
+    }
+}
