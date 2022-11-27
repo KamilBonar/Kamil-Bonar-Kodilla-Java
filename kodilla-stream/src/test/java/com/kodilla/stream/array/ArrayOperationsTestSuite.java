7deb1aaa8f236c72008a10c2dfa4cc4c2d6ea4e2
Kamil Bonar <kamilbr@op.pl>
2022-11-28 00:33:07 +0100

    7.5 Array

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.OptionalDouble;
+
+public class ArrayOperationsTestSuite {
+    private final int[] test = new int[20];
+
+    @Test
+    public void testGetAverage() {
+        for (int i = 0; i < 20; i++) {
+            test[i] = i +2;
+        }
+        //When
+        OptionalDouble average = ArrayOperations.getAverage(test);
+        //Then
+        Assertions.assertEquals(11.5, average.getAsDouble(),0.001);
+
+    }
+}
